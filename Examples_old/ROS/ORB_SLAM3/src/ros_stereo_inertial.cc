8aac540d47e86cab09e2832debd1e6c57f6caaf0
Jordan Sun <zhuoran.sun@wustl.edu>
2025-11-08 15:09:50 -0600

    Update priority to see if this would help solve the issue.

--- Examples_old/ROS/ORB_SLAM3/src/ros_stereo_inertial.cc
+++ Examples_old/ROS/ORB_SLAM3/src/ros_stereo_inertial.cc
@@ -658,7 +658,7 @@ int main(int argc, char** argv) {
   // Pthread cannot be scheduled by SCHED_DEADLINE, so we instead use
   // pthread_setschedprio and SCHED_FIFO to implement EDF_VDSD scheduling.
   struct sched_param sch_params;
-  sch_params.sched_priority = 1;
+  sch_params.sched_priority = 99;
   if (pthread_setschedparam(pthread_self(), SCHED_FIFO, &sch_params)) {
     perror("pthread_setschedparam failed");
     return 1;
@@ -805,8 +805,13 @@ int main(int argc, char** argv) {
 
   ros::AsyncSpinner spinner(4);  // Use 4 threads
   spinner.start();
+#ifdef SCHED_EDF_VDSD
+  // Main thread is idling now so set to low priority
+  if (pthread_setschedprio(pthread_self(), 1)) {
+    perror("pthread_setschedprio end main");
+  }
+#endif /* SCHED_EDF_VDSD */
   ros::waitForShutdown();
-
 #ifdef SCHED_EDF_VDSD
   // Prioritize the main thread when shutting down
   if (pthread_setschedprio(pthread_self(), 99)) {
