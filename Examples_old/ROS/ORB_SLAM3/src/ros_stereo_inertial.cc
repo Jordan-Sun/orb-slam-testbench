4b0229fec94fccfcdc8d5b20ab4976176531d62b
Jordan Sun <zhuoran.sun@wustl.edu>
2025-11-08 15:29:53 -0600

    Try updated period.

--- Examples_old/ROS/ORB_SLAM3/src/ros_stereo_inertial.cc
+++ Examples_old/ROS/ORB_SLAM3/src/ros_stereo_inertial.cc
@@ -894,7 +894,7 @@ void ImageGrabber::SyncWithImu() {
   bool initialized = false; // Creating the first map does take longer, so set start time after initialization
 
   const double maxTimeDiff = 0.01;
-  const long long period_ns = 150000000;  // 150 ms
+  const long long period_ns = 300000000;  // 300 ms
   const long long second_ns = 1000000000;  // 1 second
 
   while (1) {
