0ae2174db04d089706bd53e99edb391e0493ebfb
Jordan Sun <zhuoran.sun@wustl.edu>
2025-11-10 12:13:37 -0600

    Try new period tests.

--- src/LocalMapping.cc
+++ src/LocalMapping.cc
@@ -67,7 +67,7 @@ void LocalMapping::SetTracker(Tracking *pTracker)
 
 void LocalMapping::Run() {
   struct timespec next_iteration_time, current_time;
-  const long long period_ns = 900000000;   // 900 ms
+  const long long period_ns = 75000000;  // 75 ms
   const long long second_ns = 1000000000;  // 1 second
 
   mbFinished = false;
