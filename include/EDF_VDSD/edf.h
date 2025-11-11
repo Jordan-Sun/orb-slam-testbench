b6ddec2a64e09db9287dfe7312356d11ae9556ac
Jordan Sun <zhuoran.sun@wustl.edu>
2025-11-10 19:10:09 -0600

    Try a different period

--- include/EDF_VDSD/edf.h
+++ include/EDF_VDSD/edf.h
@@ -19,32 +19,17 @@ enum TaskID {
 // Multiple reader with no writer so we should be thread safe
 const std::vector<std::vector<int>> table_0 = {
     {
-        94,
-        88,
-        82,
+        99,
+        98,
+        97,
     },
     {
         99,
         98,
         97,
-        96,
-        95,
-        94,
-        93,
-        92,
-        91,
-        90,
-        89,
-        88,
-        87,
-        86,
-        85,
-        84,
-        83,
-        82,
     },
     {
-        82,
+        97,
     },
 };
 // Each thread should keep its own indexing to avoid race conditions
