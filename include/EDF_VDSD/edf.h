65a058b8622d8d051108a97455124b66133fc3b0
Jordan Sun <zhuoran.sun@wustl.edu>
2025-11-11 21:44:15 -0600

    Fix table

--- include/EDF_VDSD/edf.h
+++ include/EDF_VDSD/edf.h
@@ -43,22 +43,4 @@ const std::vector<std::vector<int>> table_0 = {
         88,
     },
 };
-{
-    96,
-    92,
-    88,
-},
-    {
-        99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88,
-    },
-    {
-        94,
-        88,
-    },
-// {
-//     96,
-//     92,
-//     88,
-// },
-};
 // Each thread should keep its own indexing to avoid race conditions
