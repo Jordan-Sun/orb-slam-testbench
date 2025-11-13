3070fef2b521f8b2c50519c6bf89f610bef83bf4
Jordan Sun <zhuoran.sun@wustl.edu>
2025-11-13 16:11:19 -0600

    Fix next iteration time but seems to point out period issue.

--- Examples_old/ROS/ORB_SLAM3/src/ros_stereo_inertial.cc
+++ Examples_old/ROS/ORB_SLAM3/src/ros_stereo_inertial.cc
@@ -943,7 +943,7 @@ void ImageGrabber::SyncWithImu() {
   bool initialized = false; // Creating the first map does take longer, so set start time after initialization
 
   const double maxTimeDiff = 0.01;
-  const long long period_ns = 1500000000;  // 1.5 s
+  const long long period_ns = 100000000;   // 100 ms
   const long long second_ns = 1000000000;  // 1 second
 
   while (1) {
@@ -1098,12 +1098,11 @@ void ImageGrabber::SyncWithImu() {
                 << " missed in Sync With IMU, current time "
                 << current_time.tv_sec << "." << current_time.tv_nsec
                 << std::endl;
+      next_iteration_time = current_time;
     } else {
-      std::cout << "Sync With IMU completed at " << current_time.tv_sec << "."
+      std::cout << "Sync With IMU " << current_iteration << " completed at " << current_time.tv_sec << "."
                 << current_time.tv_nsec << std::endl;
     }
-
-    next_iteration_time = current_time;
 
     // Push back the time spent and frame time
     std::pair<double, double> curr_pair =
