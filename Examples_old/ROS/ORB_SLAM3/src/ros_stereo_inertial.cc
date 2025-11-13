9083826973782ae1ce7daa9941d5e8238b04e355
Jordan Sun <zhuoran.sun@wustl.edu>
2025-11-13 13:07:16 -0600

    Remove setcpu

--- Examples_old/ROS/ORB_SLAM3/src/ros_stereo_inertial.cc
+++ Examples_old/ROS/ORB_SLAM3/src/ros_stereo_inertial.cc
@@ -724,13 +724,6 @@ int main(int argc, char** argv) {
     perror("pthread_setschedparam failed");
     return 1;
   }
-  cpu_set_t cpuset;
-  CPU_ZERO(&cpuset);
-  CPU_SET(2, &cpuset);
-  if (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset)) {
-    perror("pthread_setaffinity_np failed");
-    return 1;
-  }
   // Sync all threads to release at the same time 30 s later
   clock_gettime(CLOCK_MONOTONIC, &release_time);
   release_time.tv_sec += 30;
@@ -1009,13 +1002,6 @@ void ImageGrabber::SyncWithImu() {
   size_t prio_index = 0;
   if (pthread_setschedprio(pthread_self(), table_0[SYNC_WITH_IMU_THREAD][prio_index])) {
     perror("pthread_setschedprio syncwithimu");
-  }
-  // Migrate to CPU 5
-  cpu_set_t cpuset;
-  CPU_ZERO(&cpuset);
-  CPU_SET(5, &cpuset);
-  if (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset)) {
-    perror("pthread_setaffinity_np failed");
   }
   // Set the start time after initialization
   next_iteration_time = release_time;
